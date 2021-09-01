e9fdbb8f1ce9b811e3b52f67e0f0183ea7dc543f
NanderSantos <nandermgsdp@gmail.com>
2021-09-01 11:26:45 -0300

    Adição do projeto.

---
+++ my-spring-api/src/main/java/com/nander/spring/repository/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.nander.spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nander.spring.model.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+}
---
+++ my-spring-api/src/test/java/com/nander/spring/MySpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nander.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
